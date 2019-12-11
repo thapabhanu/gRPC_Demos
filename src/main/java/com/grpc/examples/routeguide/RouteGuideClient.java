19741bc0a777b9ac84639b7d7d5d88116f1b7023
bthapa <bhanu123_1999@yahoo.com>
2019-12-11 09:31:09 +0530

    Added grpc bi-directional stream, error handling code.

--- src/main/java/com/grpc/examples/routeguide/RouteGuideClient.java
+++ src/main/java/com/grpc/examples/routeguide/RouteGuideClient.java
@@ -1,14 +1,58 @@
 package com.grpc.examples.routeguide;
 
+import com.grpc.examples.routeguide.Point;
+import com.grpc.examples.routeguide.Rectangle;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class RouteGuideClient {
 
-    public static void main(String[] args) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
-        com.grpc.examples.routeguide.RouteGuideGrpc.RouteGuideBlockingStub stub =
-                com.grpc.examples.routeguide.RouteGuideGrpc.newBlockingStub(channel);
+    private static Logger logger = Logger.getLogger(RouteGuideClient.class.getName());
 
+    private ManagedChannel channel;
+    com.grpc.examples.routeguide.RouteGuideGrpc.RouteGuideBlockingStub blkStub;
+    com.grpc.examples.routeguide.RouteGuideGrpc.RouteGuideStub asyncStub;
+
+    public RouteGuideClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext());
+    }
+    public RouteGuideClient(ManagedChannelBuilder<?> channelBuilder) {
+        channel = channelBuilder.build();
+        blkStub = com.grpc.examples.routeguide.RouteGuideGrpc.newBlockingStub(channel);
+        asyncStub = com.grpc.examples.routeguide.RouteGuideGrpc.newStub(channel);
+    }
+
+    public void getFeatures(int lat, int lng) {
+        Point point = Point.newBuilder().setLatitude(lat).setLongitude(lng).build();
+        com.grpc.examples.routeguide.Feature feature = blkStub.getFeature(point);
+        System.out.println("Got response from GetFeatures");
+        info("Got response from GetFeatures: FeaturesName: {0}, Lat: {1},Lng:{2}", feature.getName(),
+                feature.getLocation().getLatitude(), feature.getLocation().getLongitude());
+    }
+
+    public void listFeatures(int lowLat, int lowLon, int hiLat, int hiLon){
+        info("*** ListFeatures: lowLat={0} lowLon={1} hiLat={2} hiLon={3}", lowLat, lowLon, hiLat,
+                hiLon);
+        Rectangle rectangle = Rectangle.newBuilder()
+                                .setLo(Point.newBuilder().setLatitude(lowLat).setLongitude(lowLat).build())
+                                .setHi(Point.newBuilder().setLatitude(hiLat).setLongitude(hiLon).build())
+                                .build();
+        blkStub.listFeatures(rectangle).forEachRemaining(feature -> {
+            info("Got response from listFeatures: FeaturesName: {0}, Lat: {1},Lng:{2}", feature.getName(),
+                    feature.getLocation().getLatitude(), feature.getLocation().getLongitude());
+        });
+    }
+
+    public static void main(String[] args) {
+        RouteGuideClient client = new RouteGuideClient("localhost", 50051);
+        client.getFeatures(413628156, -749015468);
+        client.listFeatures(413628156, -749015468, 419999544, -740371136);
+    }
+
+    private static void info(String msg, Object... params){
+        logger.log(Level.INFO, msg, params);
     }
 }
--- src/main/java/com/grpc/examples/routeguide/RouteGuideServiceImpl.java
+++ src/main/java/com/grpc/examples/routeguide/RouteGuideServiceImpl.java
@@ -4,6 +4,9 @@ import io.grpc.stub.StreamObserver;
 import com.grpc.examples.routeguide.*;
 import java.util.Collection;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 public class RouteGuideServiceImpl extends com.grpc.examples.routeguide.RouteGuideGrpc.RouteGuideImplBase {
 
     private Collection<com.grpc.examples.routeguide.Feature> features;
@@ -14,7 +17,8 @@ public class RouteGuideServiceImpl extends com.grpc.examples.routeguide.RouteGui
 
     @Override
     public void getFeature(com.grpc.examples.routeguide.Point request, StreamObserver<com.grpc.examples.routeguide.Feature> responseObserver) {
-
+        responseObserver.onNext(checkFeature(request));
+        responseObserver.onCompleted();
     }
 
     /**
@@ -32,8 +36,27 @@ public class RouteGuideServiceImpl extends com.grpc.examples.routeguide.RouteGui
         return com.grpc.examples.routeguide.Feature.newBuilder().setName("").setLocation(point).build();
     }
 
-    @Override
-    public void listFeatures(com.grpc.examples.routeguide.Rectangle request, StreamObserver<com.grpc.examples.routeguide.Feature> responseObserver) {
 
+    // A server-to-client streaming RPC.
+    @Override
+    public void listFeatures(com.grpc.examples.routeguide.Rectangle request,
+                             StreamObserver<com.grpc.examples.routeguide.Feature> responseObserver) {
+        int left = min(request.getLo().getLongitude(), request.getHi().getLongitude());
+        int right = max(request.getLo().getLongitude(), request.getHi().getLongitude());
+        int top = max(request.getLo().getLatitude(), request.getHi().getLatitude());
+        int bottom = min(request.getLo().getLatitude(), request.getHi().getLatitude());
+
+        for (com.grpc.examples.routeguide.Feature feature : features) {
+            if (!RouteGuideUtil.exist(feature)) {
+                continue;
+            }
+
+            int lat = feature.getLocation().getLatitude();
+            int lon = feature.getLocation().getLongitude();
+            if (lon >= left && lon <= right && lat >= bottom && lat <= top) {
+                responseObserver.onNext(feature);
+            }
+        }
+        responseObserver.onCompleted();
     }
 }
